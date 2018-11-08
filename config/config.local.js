31b23c0b7efa637d2d8bd4afa30a6693fe3c9b21
dyadyaJora <ja_massa97@mail.ru>
2018-11-08 13:37:25 +0300

    first commit, dev env, +all npm deps

---
+++ config/config.local.js
@@ -0,0 +1,8 @@
+module.exports = {
+  httpType: 'http',
+  baseHost: 'localhost',
+  webPort: 3002,
+  mongodbUri: 'mongodb://localhost/uptime-monitor',
+  rabbitPort: 5672,
+  rabbitUrl: 'amqp://localhost'
+};
