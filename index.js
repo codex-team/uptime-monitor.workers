cc500d7a650f767eb494cc41a5956106d6fec2b6
dyadyaJora <ja_massa97@mail.ru>
2018-11-13 11:16:38 +0300

    zero worker

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
-// TODO
-console.log('started');
+/**
+ * @file Worker 0 - every minute(or quant time) sends a message to run Worker1.
+ * @author dyadyaJora
+ */
+
+const amqp = require('amqplib');
+
+let config = require('./config');
+const QUANT_TIME = 5000;
+
+console.log('Worker0 STARTED');
+
+amqp.connect(config.rabbitUrl)
+  .then((connection) => {
+    return connection.createChannel();
+  })
+  .then((channel) => {
+    // Every minute send to queue INIT any message that would started WORKER1 again and again
+    setInterval(() => {
+      channel.assertQueue('init')
+        .then(() => {
+          return channel.sendToQueue('init', Buffer.alloc(1));
+        })
+        .catch((err) => {
+          console.log('something went wrong', err);
+          // TODO Error handler
+        });
+    }, QUANT_TIME);
+  })
+  .catch((err) => {
+    console.log('Connection to RABBITMQ failed, do smth', err);
+  });
---
+++ workers/worker1/index.js
@@ -0,0 +1,32 @@
+/**
+ * @file Worker 1 - create queue of projects for ping.
+ * @author dyadyaJora
+ */
+
+const amqp = require('amqplib');
+let config = require('../../config');
+
+console.log('Worker1 STARTED');
+
+amqp.connect(config.rabbitUrl)
+  .then(connection => {
+    return connection.createChannel();
+  })
+  .then(channel => {
+    channel.assertQueue('init');
+    // TODO: in future for scaling
+    // channel.prefetch(1);
+    channel.consume('init', (msg) => {
+      channel.ack(msg);
+      console.log(msg);
+      // ====================
+      // * DO foreach projects - check delay - create new queue
+      // ====================
+      // * channel.assertQueue('request');
+      // * channel.sendToQueue('request', msg);
+      // ====================
+    });
+  })
+  .catch(err => {
+    console.log(err, 'Error in worker1');
+  });
