269a64a5a51da336d2d265d395faf08fcfc7c34d
dyadyaJora <ja_massa97@mail.ru>
2018-11-16 16:01:49 +0300

    worker4 request stub

--- workers/worker4/worker4.js
+++ workers/worker4/worker4.js
@@ -2,7 +2,9 @@
  * @file describe class for Worker 4.
  */
 
+const request = require('request');
 let BaseWorker = require('../base-worker');
+let config = require('../../config');
 
 /**
  * Class repesentation a NotifyWorker
@@ -25,8 +27,24 @@ class NotifyWorker extends BaseWorker {
     let temp;
 
     switch (data.type) {
-      case 'api': { break; }
-      case 'other': { break; }
+      case 'api':
+        request({
+          method: 'POST',
+          url: config.api.postResult,
+          body: JSON.parse({
+            _id: data._id,
+            url: data.url,
+            lastPing: data.lastPing,
+            newOptions: {
+              statusContent: data.newOptions.statusContent,
+              sizeContent: data.newOptions.sizeContent,
+              delayContent: data.newOptions.delayContent
+            }
+          })
+        });
+        break;
+      case 'other':
+        break;
     }
     return new Promise((resolve, reject) => {
       resolve();
