dcda04a5e25e8bdb2cf13a64249821626fcbf28d
dyadyaJora <ja_massa97@mail.ru>
2018-12-03 10:38:30 +0300

    pm2 fix lint

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,54 +1,57 @@
+/* eslint-disable camelcase */
 module.exports = {
-  apps : [{
-    name: 'prerequest',
-    script: './workers/prerequest-worker/index.js',
-    log: 'pm2-logfile.log',
-    autorestart: true,
-    watch: true,
-    env: {
-      NODE_ENV: 'development'
+  apps: [
+    {
+      name: 'prerequest',
+      script: './workers/prerequest-worker/index.js',
+      log: 'pm2-logfile.log',
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  },
-  {
-    name: 'request',
-    script: './workers/request-worker/index.js',
-    log: 'pm2-logfile.log',
-    autorestart: true,
-    watch: true,
-    env: {
-      NODE_ENV: 'development'
+    {
+      name: 'request',
+      script: './workers/request-worker/index.js',
+      log: 'pm2-logfile.log',
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  },
-  {
-    name: 'response',
-    script: './workers/response-worker/index.js',
-    log: 'pm2-logfile.log',
-    autorestart: true,
-    watch: true,
-    env: {
-      NODE_ENV: 'development'
+    {
+      name: 'response',
+      script: './workers/response-worker/index.js',
+      log: 'pm2-logfile.log',
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     },
-    env_production: {
-      NODE_ENV: 'production'
+    {
+      name: 'notify',
+      script: './workers/notify-worker/index.js',
+      log: 'pm2-logfile.log',
+      autorestart: true,
+      watch: true,
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
-  },
-  {
-    name: 'notify',
-    script: './workers/notify-worker/index.js',
-    log: 'pm2-logfile.log',
-    autorestart: true,
-    watch: true,
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    }
-  }]
+  ]
 };
